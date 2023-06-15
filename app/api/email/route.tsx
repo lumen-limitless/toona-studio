4d47f025f3967e22f984184def51994746614b50
lumen-limitless <lumenlimitless@gmail.com>
2023-06-14 23:54:00 -0400

    contact form

--- app/api/email/route.tsx
+++ app/api/email/route.tsx
@@ -13,9 +13,7 @@ export async function POST(req: NextRequest) {
 
   // Configure the email transport using the SMTP server of your choice
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: process.env.SMTP_SECURE === 'true',
+    service: 'gmail',
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASSWORD,
