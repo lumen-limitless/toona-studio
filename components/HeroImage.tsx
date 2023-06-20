5ebad32f03737c3840d7c94cf2303a0bfba338f5
lumen-limitless <lumenlimitless@gmail.com>
2023-06-19 22:55:12 -0400

    update animations ✨

--- components/HeroImage.tsx
+++ components/HeroImage.tsx
@@ -1,11 +1,17 @@
 'use client'
 import Image from 'next/image'
 import heroPhoto from 'public/hero.png'
+import { motion } from 'framer-motion'
 
 export const HeroImage: React.FC = () => {
   return (
     <div className="mx-auto flex w-full max-w-[567px] items-center justify-center lg:mx-0">
-      <div className="relative flex h-[399.57px] w-[209px] items-center justify-center rounded-b-[400px] rounded-t-[400px] md:h-[558.5px] md:w-[292.13px] xl:h-[759px] xl:w-[397px]">
+      <motion.div
+        initial={{ opacity: 0, translateX: 20 }}
+        animate={{ opacity: 1, translateX: 0 }}
+        transition={{ duration: 0.3, ease: 'easeInOut', delay: 0 }}
+        className="relative flex h-[399.57px] w-[209px] items-center justify-center rounded-b-[400px] rounded-t-[400px] md:h-[558.5px] md:w-[292.13px] xl:h-[759px] xl:w-[397px]"
+      >
         <Image
           src={heroPhoto}
           alt=""
@@ -19,7 +25,7 @@ export const HeroImage: React.FC = () => {
           data="/orbit.svg"
           className="absolute top-10 -mr-[6px] w-[380px] md:top-14 md:w-[540px] xl:top-[113.5px] xl:w-[750px]"
         />
-      </div>
+      </motion.div>
     </div>
   )
 }
--- components/Landing.tsx
+++ components/Landing.tsx
@@ -12,30 +12,41 @@ export default function Landing() {
       className="min-h-[calc(100vh-100px)] overflow-x-hidden px-5 pb-12 md:px-20 xl:px-[269px]"
     >
       <div className="mx-auto flex w-full max-w-container flex-col-reverse gap-12 lg:flex-row">
-        <motion.div
-          className="flex flex-col justify-center gap-12"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.3, ease: 'easeInOut' }}
-        >
-          <h1 className="text-4xl font-kindabold md:text-[52px]/[71px] lg:text-7xl">
+        <div className="flex flex-col justify-center gap-12">
+          <motion.h1
+            className="text-4xl font-kindabold md:text-[52px]/[71px] lg:text-7xl"
+            initial={{ opacity: 0, translateX: -20 }}
+            animate={{ opacity: 1, translateX: 0 }}
+            transition={{ duration: 0.3, ease: 'easeInOut' }}
+          >
             Crafting joy <br className="md:hidden" /> through{' '}
             <br className="hidden md:block" /> digital{' '}
             <br className="md:hidden" /> experiences.
-          </h1>
-          <p className="text-lg font-kindabold lg:text-2xl">
+          </motion.h1>
+          <motion.p
+            className="text-lg font-kindabold lg:text-2xl"
+            initial={{ opacity: 0, translateX: -20 }}
+            animate={{ opacity: 1, translateX: 0 }}
+            transition={{ duration: 0.3, ease: 'easeInOut', delay: 0.1 }}
+          >
             We are a digital product studio based in Rochester, NY{' '}
             <br className="hidden lg:inline-block" /> that designs & develops
             innovative and human-centered <br className="hidden md:block" />{' '}
             experiences to enrich your business.
-          </p>
-          <Link
-            href="/contact"
-            className="mx-auto inline-flex h-16 w-full items-center justify-center rounded-full bg-indigo-500 text-xl font-kindabold text-light transition-colors duration-300 ease-in-out hover:bg-indigo-600 md:mx-0 md:max-w-[280px]"
+          </motion.p>
+          <motion.div
+            initial={{ opacity: 0, translateX: -20 }}
+            animate={{ opacity: 1, translateX: 0 }}
+            transition={{ duration: 0.3, ease: 'easeInOut', delay: 0.2 }}
           >
-            Let&apos;s chat
-          </Link>
-        </motion.div>
+            <Link
+              href="/contact"
+              className="mx-auto inline-flex h-16 w-full items-center justify-center rounded-full bg-indigo-500 text-xl font-kindabold text-light transition-colors duration-300 ease-in-out hover:bg-indigo-600 md:mx-0 md:max-w-[280px]"
+            >
+              Let&apos;s chat
+            </Link>
+          </motion.div>
+        </div>
 
         <HeroImage />
       </div>
