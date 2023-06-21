381d7124048bfd526032f42552b75659b2f360f5
lumen-limitless <lumenlimitless@gmail.com>
2023-06-20 21:26:11 -0400

    improved hero img rings position 🖼️

--- components/HeroImage.tsx
+++ components/HeroImage.tsx
@@ -3,29 +3,26 @@ import Image from 'next/image'
 import heroPhoto from 'public/hero.png'
 import { motion } from 'framer-motion'
 
-export const HeroImage: React.FC = () => {
+export const HeroImage = () => {
   return (
-    <div className="mx-auto flex w-full max-w-[567px] items-center justify-center lg:mx-0">
-      <motion.div
-        initial={{ opacity: 0, translateX: 20 }}
-        animate={{ opacity: 1, translateX: 0 }}
-        transition={{ duration: 0.6, ease: 'easeInOut', delay: 0 }}
-        className="relative flex h-[399.57px] w-[209px] items-center justify-center rounded-b-[400px] rounded-t-[400px] md:h-[558.5px] md:w-[292.13px] xl:h-[759px] xl:w-[397px]"
-      >
-        <Image
-          src={heroPhoto}
-          alt=""
-          priority
-          quality={100}
-          fill
-          className="object-contain object-center"
-        />
+    <motion.div
+      className="mx-auto flex w-full max-w-[567px] items-center justify-center lg:mx-0"
+      initial={{ opacity: 0, translateX: 20 }}
+      animate={{ opacity: 1, translateX: 0 }}
+      transition={{
+        duration: 0.6,
+        ease: 'easeInOut',
+        delay: 0,
+      }}
+    >
+      <div className="relative -top-[0.5px] flex h-[400px] w-[210px] rounded-[400px] md:h-[558.5px] md:w-[292.13px] xl:h-[759px] xl:w-[397px]">
+        <Image src={heroPhoto} alt="" priority quality={100} fill />
         <object
           type="image/svg+xml"
           data="/orbit.svg"
-          className="absolute top-10 -mr-[6px] w-[380px] md:top-14 md:w-[540px] xl:top-[113.5px] xl:w-[750px]"
+          className="absolute -left-[87.92px] top-[12%] w-[392px] md:-left-[122.88px] md:w-[547.83px] xl:-left-[167px] xl:w-[744.5px]"
         />
-      </motion.div>
-    </div>
+      </div>
+    </motion.div>
   )
 }
