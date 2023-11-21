f5588be2f6d5e8e27973c9c3cb52c86cf2025493
lumen-limitless <lumenlimitless@gmail.com>
2023-11-21 13:13:55 -0500

    Fix typo in baseUrl variable

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { type MetadataRoute } from 'next'
 
 const baseUrl = process.env.NEXT_PUBLIC_APP_URL
-  ? `https://${process.env.NEXT_PUBLIC_baseUrl}`
+  ? `https://${process.env.NEXT_PUBLIC_APP_URL}`
   : process.env.NEXT_PUBLIC_VERCEL_URL
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : 'http://localhost:3000'
